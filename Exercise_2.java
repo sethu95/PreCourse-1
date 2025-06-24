212084ccf6be7522adbe2aa7c709b6fe4674c81d
Sethu Manickam <sethu.sxcs@gmail.com>
2025-06-24 16:34:24 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,7 @@
+// Time Complexity: O(1)
+// Space Complexity: O(n)
+// Approach: Store it in a reversed linked list, and keep adding elements to the front of the linkedlist
+
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -8,31 +12,45 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        return root == null;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        StackNode node = new StackNode(data);
+        node.next = root;
+        root = node;
     } 
   
     public int pop() 
     { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
+	    //If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
-	//Also return the popped element 
+	    //Also return the popped element 
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int poppedNode = root.data;
+            root = root.next;
+            return poppedNode;
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if (isEmpty()) return 0;
+        else return root.data;
     } 
   
 	//Driver code
@@ -45,7 +63,8 @@ public class StackAsLinkedList {
         sll.push(20); 
         sll.push(30); 
   
-        System.out.println(sll.pop() + " popped from stack"); 
+        System.out.println(sll.pop() + " popped from stack");
+        System.out.println(sll.pop() + " popped from stack");
   
         System.out.println("Top element is " + sll.peek()); 
     } 
