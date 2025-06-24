26bb4fe8372f7992b2fc2d16a68d1b81bfd4be53
Sethu Manickam <sethu.sxcs@gmail.com>
2025-06-24 15:42:33 -0700

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,48 @@
+// Time Complexity: O(1)
+// Space Complexity: O(n)
+// Approach: Use top to denote the top of the stack
+// While pushing, keep incrementing top, and popping keep decrementing top
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
+    //Kindly include Time and Space complexity at top of each file
+    static final int MAX = 5; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        return top == -1;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
-        //Write your code here
+        if (top == MAX-1) {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        a[++top] = x;
+        return true;
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+
+        return a[top--];
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if (!isEmpty()) return a[top];
+        else return 0;
     } 
 } 
   
@@ -41,6 +54,11 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
+        s.push(40); 
+        s.push(50);
+        s.push(60);
+        System.out.println(s.pop() + " Popped from stack"); 
+        s.push(60);
         System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
